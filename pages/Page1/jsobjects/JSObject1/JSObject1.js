bf40549ee358c740099728edcb19eb9156f7f6c0
shadab <shadab@appsmith.com>
2024-01-03 05:30:17 +0000

    commit

---
+++ pages/Page1/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,11 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		//setInterval(() => {showAlert('What?')}, 2000, 'Interval1')
+		
+	},
+	async myFun2 () {
+		//clearInterval('Interval1')
+	}
+}
